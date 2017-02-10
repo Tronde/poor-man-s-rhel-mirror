099a5247c5de3751156cfda314f9f577327143c6
Tronde <tronde@my-it-brain.de>
2017-02-10 13:07:07 +0100

    Fixed issue with DOWNLOAD_PATH

--- do_reposync.sh
+++ do_reposync.sh
@@ -10,19 +10,19 @@
 # License: MIT copyright (c) 2016 Joerg Kastning <joerg.kastning(aet)uni-bielefeld(dot)de>
 
 LOG="/var/log/do_reposync.log"
-REPOID=(rhel-7-server-rpms)
-DOWNLOADPATH="/var/www/html/local-rhel-7-repo"
+REPOID=(rhel-7-server-rpms rhel-server-rhscl-7-rpms rhel-7-server-optional-rpms rhel-7-server-extras-rpms rhel-7-server-thirdparty-oracle-java-rpms rhel-7-server-supplementary-rpms)
+DOWNLOADPATH="/rpm-repo"
 
 echo \# `date +%Y-%m-%dT%H:%M` - START REPOSYNC \# > $LOG
 
 for REPO in "${REPOID[@]}"
   do
-    reposync --gpgcheck -l --repoid=$REPO --download_path=/var/www/html/$DOWNLOADPATH --downloadcomps --download-metadata -n >> $LOG
-    cd /var/www/html/$DOWNLOADPATH/$REPO
+    reposync --gpgcheck -l --repoid=$REPO --download_path=$DOWNLOADPATH --downloadcomps --download-metadata -n >> $LOG
+    cd $DOWNLOADPATH/$REPO
     if [[ -e comps.xml ]]; then
-      createrepo -v /var/www/html/$DOWNLOADPATH/$REPO -g comps.xml >> $LOG
+      createrepo -v $DOWNLOADPATH/$REPO -g comps.xml >> $LOG
     else
-      createrepo -v /var/www/html/$DOWNLOADPATH/$REPO >> $LOG
+      createrepo -v $DOWNLOADPATH/$REPO >> $LOG
     fi
 done
 
