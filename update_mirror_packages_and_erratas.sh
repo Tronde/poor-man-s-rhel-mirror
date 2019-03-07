298d37928f3be313bcdfadbb2ec8cc5260afc93b
Tronde <3941069+Tronde@users.noreply.github.com>
2019-03-07 05:59:09 +0100

    Use updateinfo.xml from mirrored repo instead of from the cache of the host and added error logging

--- update_mirror_packages_and_erratas.sh
+++ update_mirror_packages_and_erratas.sh
@@ -15,28 +15,40 @@ PROGDIR=$(dirname $(readlink -f ${0}))
 . $PROGDIR/CONFIG
 
 # Functions #################################################################
+remove_older_updateinfo()
+{
+  for REPO in "${REPOID[@]}"
+    do
+      /usr/bin/find ${BASEDIR}${REPO}/ -type f -mtime +1 -name "*updateinfo.xml.gz" -exec rm {} \;
+  done
+}
 deploy_updateinfo()
 {
-  /usr/bin/yum clean all
-  /usr/bin/yum list-sec
-  for REPO in "${LOCALREPOS[@]}"
+  for REPO in "${REPOID[@]}"
     do
-       /usr/bin/rm $REPODIR/$REPO/repodata/*updateinfo*
-       /usr/bin/cp $CACHEDIR/*-updateinfo.xml.gz $REPODIR/$REPO/repodata/$TARGETGZFILENAME
-       /usr/bin/gzip -d $REPODIR/$REPO/repodata/$TARGETGZFILENAME
-       /usr/bin/modifyrepo $REPODIR/$REPO/repodata/$TARGETFILENAME $REPODIR/$REPO/repodata/
+			for FILE in ${BASEDIR}${REPO}/*-updateinfo.xml.gz
+				do
+					[[ $FILE -nt $LATEST ]] && LATEST=$FILE
+			done
+      /usr/bin/rm ${BASEDIR}${REPO}/repodata/*updateinfo*
+      /usr/bin/cp ${LATEST} ${BASEDIR}${REPO}/repodata/$TARGETGZFILENAME
+      /usr/bin/gzip -d ${BASEDIR}${REPO}/repodata/$TARGETGZFILENAME
+      /usr/bin/modifyrepo ${BASEDIR}${REPO}/repodata/$TARGETFILENAME ${BASEDIR}${REPO}/repodata/
+			unset -v LATEST
+			unset -v FILE
   done
 }
 
 # Main ##############################################################
 echo \# `date +%Y-%m-%dT%H:%M` - Update Local Mirror \# > $LOG
-bash $PROGDIR/do_reposync.sh >> $LOG
+bash $PROGDIR/do_reposync.sh >> $LOG 2>&1
 
 echo "\n" >> $LOG
-echo \# `date +%Y-%m-%dT%H:%M` - Update rhel-STAGE-repositories \# >> $LOG
-bash $PROGDIR/update_multiple_stages.sh >> $LOG
-
 echo \# `date +%Y-%m-%dT%H:%M` - Implement Errata-Information \# >> $LOG
-deploy_updateinfo >> $LOG
+remove_older_updateinfo >> $LOG 2>&1
+deploy_updateinfo >> $LOG 2>&1
+echo "\n" >> $LOG
+echo \# `date +%Y-%m-%dT%H:%M` - Update rhel-STAGE-repositories \# >> $LOG
+bash $PROGDIR/update_multiple_stages.sh >> $LOG 2>&1
 echo \# `date +%Y-%m-%dT%H:%M` - End of processing \# >> $LOG
 # End ###############################################################
