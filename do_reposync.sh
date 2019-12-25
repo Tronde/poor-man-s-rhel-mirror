c483f0923c168535ec5058088002d5aeeee714f9
root <root@rhel8-dev.vm>
2019-12-25 20:46:56 +0100

    Make it fit for RHEL 8

--- do_reposync.sh
+++ do_reposync.sh
@@ -19,13 +19,7 @@ echo \# `date +%Y-%m-%dT%H:%M` - START REPOSYNC \# > $LOG
 
 for REPO in "${REPOID[@]}"
   do
-    reposync --gpgcheck -l --repoid=$REPO --download_path=$BASEDIR --downloadcomps --download-metadata -n >> $LOG
-    cd $BASEDIR/$REPO
-    if [[ -e comps.xml ]]; then
-      createrepo -v $BASEDIR/$REPO -g comps.xml >> $LOG
-    else
-      createrepo -v $BASEDIR/$REPO >> $LOG
-    fi
+    reposync --repoid=$REPO --download-path=$BASEDIR --downloadcomps --download-metadata -n >> $LOG
 done
 
 echo \# `date +%Y-%m-%dT%H:%M` - END REPOSYNC \# >> $LOG
