d68793f0f46510c348b988c8f8170338192f4035
Joerg Kastning <joerg.kastning@uni-bielefeld.de>
2022-06-29 15:26:39 +0200

    Update name for rhel9
    Signed-off-by: Joerg Kastning <joerg.kastning@uni-bielefeld.de>

--- update_rhel-e-stage.sh
+++ update_rhel-e-stage.sh
@@ -2,6 +2,6 @@
 # Wrapper-Script to update the rhel-e-stage
 # License: MIT copyright (c) 2019 Joerg Kastning <joerg.kastning(aet)uni-bielefeld(dot)de>
 PROGDIR=$(dirname $(readlink -f ${0}))
-$PROGDIR/rsync_repo.sh -Q rhel-8-for-x86_64-baseos-rpms -Z rhel-e-stage-baseos
-$PROGDIR/rsync_repo.sh -Q rhel-8-for-x86_64-appstream-rpms -Z rhel-e-stage-appstream
+$PROGDIR/rsync_repo.sh -Q rhel-9-for-x86_64-baseos-rpms -Z rhel-e-stage-baseos
+$PROGDIR/rsync_repo.sh -Q rhel-9-for-x86_64-appstream-rpms -Z rhel-e-stage-appstream
 exit
