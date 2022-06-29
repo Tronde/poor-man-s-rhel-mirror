651dd84b1dbe73f2c73e81dd9ea0fe32da758e05
Joerg Kastning <joerg.kastning@uni-bielefeld.de>
2022-06-29 15:31:22 +0200

    Fixed syntax error and removed obsolete code.
    Signed-off-by: Joerg Kastning <joerg.kastning@uni-bielefeld.de>

--- do_reposync.sh
+++ do_reposync.sh
@@ -19,17 +19,7 @@ echo \# `date +%Y-%m-%dT%H:%M` - START REPOSYNC \# > $LOG
 
 for REPO in "${REPOID[@]}"
   do
-<<<<<<< HEAD
     reposync --gpgcheck -l --repoid=$REPO --download_path=$BASEDIR --download-metadata -n >> $LOG
-    cd $BASEDIR/$REPO
-    if [[ -e comps.xml ]]; then
-      createrepo -v $BASEDIR/$REPO -g comps.xml >> $LOG
-    else
-      createrepo -v $BASEDIR/$REPO >> $LOG
-    fi
-=======
-    reposync --repoid=$REPO --download-path=$BASEDIR --downloadcomps --download-metadata -n >> $LOG
->>>>>>> 471d2bda4bcff8ded93c5b49c2621e0a2b86090a
 done
 
 echo \# `date +%Y-%m-%dT%H:%M` - END REPOSYNC \# >> $LOG
